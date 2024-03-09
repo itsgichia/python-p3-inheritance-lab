a72e73b701a84cd65a40e57229a977981210a57b
itsgichia <gichiamuiruri@gmail.com>
2024-03-10 00:21:09 +0300

    Task Completed

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,15 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    #def list_knowledge(self, knowledge):
+        #self.knowledge = knowledge
+
+
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+
